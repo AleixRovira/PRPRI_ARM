4b95a896a59544dc7ae34c85370d15f6df3887e0
Aleix Rovira <aleix.rovira@students.salle.url.edu>
2025-06-05 13:11:16 +0200

    Menu principal

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void menu()
+{
+
+    printf("------------------------\n");
+    printf("---LS Electronics Hub---\n");
+    printf("------------------------\n\n");
+
+    int option = 0;
+    while (option != 5)
+    {
+        printf("\t1. Shop Register\n");
+        printf("\t2. User Register\n");
+        printf("\t3. Staff Access\n");
+        printf("\t4. Client Login\n");
+        printf("\t5. Exit\n");
+        printf("Option: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            printf("\nYou selected Shop Register.\n");
+            // Call function for shop register here
+            break;
+        case 2:
+            printf("\nYou selected User Register.\n");
+            // Call function for user register here
+            break;
+        case 3:
+            printf("\nYou selected Staff Access.\n");
+            // Call function for staff access here
+            break;
+        case 4:
+            printf("\nYou selected Client Login.\n");
+            // Call function for client login here
+            break;
+        case 5:
+            printf("\nTank you. Exiting the program.\n");
+            break;
+        default:
+            printf("\nERROR: Invalid option.\n");
+        }
+    }
+}
+
+int main()
+{
+
+    menu();
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -4,6 +4,13 @@
 operations.o: module/operations.c
 	gcc -c module/operations.c -ggdb
 
+main.o: main.c
+	gcc -c main.c -ggdb
+
+prod: main.o operations.o
+	gcc main.o operations.o -o prpr.exe -ggdb -lm
+	rm *.o
+
 #TDD
 TEST_operations.o: TDD/TEST_operations.c
 	gcc -c TDD/TEST_operations.c -ggdb
@@ -11,6 +18,8 @@ TEST_operations.o: TDD/TEST_operations.c
 TEST_operations: operations.o TEST_operations.o
 	gcc operations.o TEST_operations.o -o test.exe -ggdb -lm
 	rm *.o
+
+test: TEST_operations
 
 #Clean
 clean:
