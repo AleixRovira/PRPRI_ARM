e1458319874dbd3e23bf8cc6664fb1117da7afb8
Aleix Rovira <aleix.rovira@students.salle.url.edu>
2025-06-11 15:49:03 +0200

    Se verifican los datos

--- module/global.c
+++ module/global.c
@@ -13,3 +13,77 @@ int GLOBAL_printLineInFile(char *filename, char *line)
     fclose(file);
     return 0;
 }
+
+int GLOBAL_validatePassword(char* password) {
+    int uppercase = 0, lowercase = 0, special = 0, length = 0;
+
+    for (int i = 0; password[i] != '\0'; i++) {
+        length++;
+
+        if (password[i] >= 'A' && password[i] <= 'Z') {
+            uppercase = 1;
+        }
+
+        else if (password[i] >= 'a' && password[i] <= 'z') {
+            lowercase = 1;
+        }
+
+        else if ((password[i] >= '!' && password[i] <= '/') || (password[i] >= ':' && password[i] <= '@') || (password[i] >= '[' && password[i] <= '`') || (password[i] >= '{' && password[i] <= '~')) {
+            special = 1;
+        }
+    }
+
+	if (length >= 8 && uppercase && lowercase && special) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+char GLOBAL_isValidEmailChar(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_' || c == '-');
+}
+
+char GLOBAL_validateEmail(char *email) {
+    char valid = 0;
+    int at_count = 0;
+    int at_position = -1;
+    char dot_count = 0;
+    int length = strlen(email);
+
+    if (length == 0) {
+		valid = 0;
+    }
+
+    for (int i = 0; i < length; i++) {
+        switch (email[i]) {
+            case '@':
+                at_count++;
+
+				if (at_count > 1) {
+				    valid = 0;
+				}
+                
+				at_position = i;
+                break;
+            case '.':
+                if (at_position != -1 && i > at_position) {
+                    dot_count = 1;
+                }
+
+                break;
+            default:
+                if (!GLOBAL_isValidEmailChar(email[i])) {
+                    return (0);
+                }
+
+                break;
+        }
+    }
+    
+    if (at_count == 1 && at_position > 1 && at_position < length - 3 && dot_count) {
+        valid = 1;
+    }
+
+    return valid;
+}
--- module/staff.c
+++ module/staff.c
@@ -7,23 +7,55 @@ void STAFF_register()
     printf("\tEnter staff name: ");
     scanf("%ms", &staff.name);
 
-    printf("\tEnter staff email: ");
-    scanf("%ms", &staff.email);
+    do
+    {
+        printf("\tEnter staff email: ");
+        scanf("%ms", &staff.email);
+        if (!GLOBAL_validateEmail(staff.email))
+        {
+            printf("\nERROR: Invalid email.  Please enter a valid email.\n");
+            free(staff.email);
+            staff.email = NULL;
+        }
+    }while(staff.email == NULL);
 
-    printf("\tEnter staff password: ");
-    scanf("%ms", &staff.password);
+    do
+    {
+        printf("\tEnter staff password: ");
+        scanf("%ms", &staff.password);
+        if (!GLOBAL_validatePassword(staff.password))
+        {
+            printf("\nERROR: Invalid password.\nPassword must be:\n- At least 8 characters long.\n- Contain at least one uppercase letter.\n- Contain at least one lowercase letter.\n- Contain at least one special character.\n");
+            free(staff.password);
+            staff.password = NULL;
+        }
+    } while (staff.password == NULL);
 
-    printf("\tEnter shop code: ");
-    scanf("%ms", &staff.shop_code);
+    Shop shop;
+    do
+    {
+        printf("\tEnter shop code: ");
+        scanf("%ms", &staff.shop_code);
+        shop = SHOP_findShopByCode(staff.shop_code);
+        if (shop.name == NULL)
+        {
+            printf("\nERROR: Shop code does not exist. Please enter a valid shop code.\n");
+            free(staff.shop_code);
+            staff.shop_code = NULL;
+        }
+    } while (staff.shop_code == NULL);
+    free(shop.name);
+    free(shop.address);
+    free(shop.phone);
+    free(shop.email);
+    free(shop.code);
 
-    
-    printf("\nStaff registered successfully:\n");
-    printf("Name: %s\n", staff.name);
-    printf("Email: %s\n", staff.email);
-    printf("Shop Code: %s\n", staff.shop_code);
+    //Guardar en el fichero
 
     free(staff.name);
     free(staff.email);
     free(staff.password);
     free(staff.shop_code);
+
+    printf("\nStaff registered successfully:\n");
 }
